0dc73bddf0fd92ac1ad72cb06907030439071e1a
Sascha Siemer <sascha.siemer@mediatack.de>
2015-09-29 18:22:42 +0200

    updated panorama

--- panorama360/js/script.js
+++ panorama360/js/script.js
@@ -7,78 +7,169 @@ $(document).ready(function(){
 	var scene = createScene();
 
 	engine.loadingUIText = "Bitte haben Sie einen Moment Geduld, die benötigten Daten werden geladen...";
-
 	engine.runRenderLoop(function () {
 		scene.render();
 	});
 
-	window.addEventListener("resize", function () { engine.resize();});
+	// event listeners
+	window.addEventListener("resize", function () {
+		engine.resize();
+	});
+	window.addEventListener("click", function () {
+		// We try to pick an object
+		var pickResult = scene.pick(scene.pointerX, scene.pointerY);
+		console.log(Math.round(pickResult.pickedPoint.x) + ', ' + Math.round(pickResult.pickedPoint.y) + ', ' + Math.round(pickResult.pickedPoint.z));
+	});
 
 });
 
 function createScene() {
-	var image;
+	var environmentSphere, image;
+	var doors = [];
 	var hashes = location.hash.split('#');
 
+	// create the scene
 	var scene = new BABYLON.Scene(engine);
 	scene.clearColor = new BABYLON.Color3(0.9, 0.9, 0.9);
 
+	// check for parameters and define doors
 	switch(hashes[1]){
-		case 'quality1':
-			image = "img/_61U9543-Panorama_equi-2_tiny.jpg";
+		case 'scene1':
+			image = "img/app7.jpg";
+			doors.push({
+				position: new BABYLON.Vector3(214, -24, -464),
+				target: 'scene2'
+			});
+			doors.push({
+				position: new BABYLON.Vector3(494, -22, 130),
+				target: 'scene3'
+			});
 			break;
-		case 'quality2':
-			image = "img/_61U9543-Panorama_equi-2_small.jpg";
+		case 'scene2':
+			image = "img/app7_kitchen.jpg";
+			doors.push({
+				position: new BABYLON.Vector3(228, 59, 454),
+				target: 'scene1'
+			});
+			doors.push({
+				position: new BABYLON.Vector3(510, 19, -16),
+				target: 'scene4'
+			});
 			break;
-		case 'quality3':
-			image = "img/_61U9543-Panorama_equi-2_large.jpg";
+		case 'scene3':
+			image = "img/garden1.jpg";
+			doors.push({
+				position: new BABYLON.Vector3(322, 305, -255),
+				target: 'scene1'
+			});
+			doors.push({
+				position: new BABYLON.Vector3(448, 31, 244),
+				target: 'scene4'
+			});
+			break;
+		case 'scene4':
+			image = "img/garden2.jpg";
+			doors.push({
+				position: new BABYLON.Vector3(-442, -47, -253),
+				target: 'scene3'
+			});
+			doors.push({
+				position: new BABYLON.Vector3(61, 394, -320),
+				target: 'scene1'
+			});
+			doors.push({
+				position: new BABYLON.Vector3(211, 337, -322),
+				target: 'scene2'
+			});
 			break;
 		default:
-			image = "img/_61U9543-Panorama_equi-2_small.jpg"
+			image = "img/app7.jpg"
 	}
 
+	// define what assets to load and what objects to add
 	var assetsManager = new BABYLON.AssetsManager(scene);
 	var textureTask = assetsManager.addTextureTask("image task", image);
 	textureTask.onSuccess = function(task) {
+		// check for debug parameter
 		if(hashes[1] == 'debug' || hashes[2] == 'debug') {
 			scene.debugLayer.show();
 		}
 
-		var sphere1 = BABYLON.Mesh.CreateSphere("sphere1", 32, 256, scene);
+		// create the environment sphere
+		environmentSphere = BABYLON.Mesh.CreateSphere("environmentSphere", 32, 1024, scene);
 
-		//var sphere2 = BABYLON.Mesh.CreateSphere("sphere1", 32, 5, scene);
-		//sphere2.position = new BABYLON.Vector3(0, 0, 10);
-
-		var material = new BABYLON.StandardMaterial("texture1", scene);
+		var material = new BABYLON.StandardMaterial("environmentMaterial", scene);
 		material.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
 		material.diffuseTexture = task.texture;
 		material.diffuseTexture.wAng = -Math.PI/2;
 		material.diffuseTexture.vAng = Math.PI;
-		material.diffuseTexture.level = 1.5;
-
-		// material.diffuseTexture.coordinatesMode = BABYLON.Texture.SPHERICAL_MODE;
+		material.diffuseTexture.level = 1.4;
 		material.backFaceCulling = false;
-		sphere1.material = material;
+		// material.diffuseTexture.coordinatesMode = BABYLON.Texture.SPHERICAL_MODE;
+
+		// assign material to environment sphere
+		environmentSphere.material = material;
 		//sphere1.infiniteDistance = true;
 
-		scene.registerBeforeRender(function(){
-			//material.diffuseTexture.vOffset += 0.00025;
-			sphere1.rotation.y += 0.001;
-		});
+		// create doors
+		if(doors.length > 0) {
+			var spriteManagerDoors = new BABYLON.SpriteManager("doorsManager","img/target.png", 10, 51, scene);
+
+
+			var doorMaterial = new BABYLON.StandardMaterial("doorMaterial", scene);
+			doorMaterial.emissiveColor = new BABYLON.Color3(0.2, 0.9, 0.2);
+
+			for(var i=0; i<doors.length; i++){
+				var doorSphere = BABYLON.Mesh.CreateSphere("door1", 12, 32, scene);
+				var door = new BABYLON.Sprite("player", spriteManagerDoors);
+				door.size = 51;
+				door.position = doors[i].position.subtract(doors[i].position.scale(0.05));
+				door.parent = environmentSphere;
+
+				doorSphere.position = doors[i].position;
+				doorSphere.material = doorMaterial;
+
+				// define door actions
+				door.actionManager = new BABYLON.ActionManager(scene);
+				setDoorAction(door, doors[i].target);
+			}
+
+		}
+
 	};
 
-	var camera = new BABYLON.ArcRotateCamera("camera1", Math.PI/2, Math.PI/2-0.3, 150, new BABYLON.Vector3(0, 0, 0), scene);
-	camera.setTarget(BABYLON.Vector3.Zero());
+	// camera
+	var camera = new BABYLON.ArcRotateCamera("camera1", Math.PI/2, Math.PI/2-0.3, 250, new BABYLON.Vector3(0, 10, 0), scene);
 	camera.attachControl(canvas, true);
-
 	camera.lowerRadiusLimit = 10;
-	camera.upperRadiusLimit = 96;
+	camera.upperRadiusLimit = 256;
 
-	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-	light.intensity = 1.1;
+	// light
+	var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+	var light2 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, -1, 0), scene);
+	light1.intensity = 0.5;
+	light2.intensity = 0.5;
 
+	// start the loading process
 	assetsManager.load();
+
+	// stuff to do before each render call
+	scene.registerBeforeRender(function(){
+		// auto rotation
+		//material.diffuseTexture.vOffset += 0.00025;
+		if(environmentSphere) {
+			//environmentSphere.rotation.y += 0.001;
+		}
+	});
 
 	return scene;
 
+}
+
+function setDoorAction(door, target){
+	door.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickUpTrigger, function () {
+		var path = 'http://' + window.location.hostname + window.location.pathname;
+		window.location.href = path + '#' + target;
+		location.reload();
+	}));
 }
